aa3627cc6544cca471ca9bc56f240dcbc02ad1ba
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-02 23:16:06 +0400

    Creating Income and Expense

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,19 @@
   <Header/>
   <div class="container">
   <Balance/>
+  <IncomeExpense/>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
-
+import IncomeExpense from "./components/IncomeExpenses.vue";
 export default  {
     components : {
         Header,
-        Balance
+        Balance,
+        IncomeExpense
     }
 }
 </script>
