e7207c0133849da75e63515fb9a098a616ae41dd
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-02 23:56:07 +0400

    Adding functionality to TransactionList

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,13 +1,31 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-       <li class="minus">
+        <li v-for="transaction in transactions" :key="transaction.id">
+            
+        </li>
+       <!-- <li class="minus">
         Cash <span>-400$</span> <button
        class="delete-btn" >x</button>
        </li>
        <li class="plus">
         Paycheck <span>800$</span> <button
        class="delete-btn" >x</button>
-       </li>
+       </li> -->
     </ul>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+         transactions: [
+   { id: 1, text: 'Flower', amount: -20 },
+   { id: 2, text: 'Salary', amount: 300 },
+  { id: 3, text: 'Book', amount: -10 },
+   { id: 4, text: 'Camera', amount: 150 },
+            ]
+        }
+    }
+}
+</script>
