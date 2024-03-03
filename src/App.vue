98a9338f7d96b7c4bdda8fa9184e57332d6466bf
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-03 23:30:03 +0400

    Creating functional income and expense

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <Header/>
   <div class="container">
   <Balance :total="total"/>
-  <IncomeExpense/>
+  <IncomeExpense :income="+income" :expenses="+expenses"/>
   <TransactionList :transactions="transactions"/>
   <AddTransaction/>
   </div>
@@ -27,6 +27,19 @@ const total = computed (() => {
   return transactions.value.reduce((acc, transaction) => {
     return acc + transaction.amount;
   },0);
-})
+});
 
+const income = computed(() => {
+  return transactions.value
+  .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0)
+    .toFixed(2);
+});
+
+const expenses = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0)
+    .toFixed(2);
+  });
 </script>
