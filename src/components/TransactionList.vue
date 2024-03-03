8766ebfe6a2ab969d6ad963bd0c93797578ab1e6
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-03 22:59:22 +0400

    Adding functionality to transaction

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,17 +1,13 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-        <li v-for="transaction in transactions" :key="transaction.id">
-            
+        <li 
+        v-for="transaction in transactions" 
+        :key="transaction.id" 
+        :class="transaction.amount < 0 ? 'minus' :'plus'">
+            {{transaction.text}} <span>${{transaction.amount }}</span> <button
+            class="delete-btn" >x</button>    
         </li>
-       <!-- <li class="minus">
-        Cash <span>-400$</span> <button
-       class="delete-btn" >x</button>
-       </li>
-       <li class="plus">
-        Paycheck <span>800$</span> <button
-       class="delete-btn" >x</button>
-       </li> -->
     </ul>
 </template>
 
