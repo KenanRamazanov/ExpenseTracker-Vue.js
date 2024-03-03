bb58e720a9692918a90224df1a0f9b5f88b96327
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-03 23:14:32 +0400

    Editing props

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,24 @@
   <div class="container">
   <Balance/>
   <IncomeExpense/>
-  <TransactionList/>
+  <TransactionList :transactions="transactions"/>
   <AddTransaction/>
   </div>
 </template>
 
-<script>
+<script setup>
 import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
 import IncomeExpense from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from './components/AddTransaction.vue';
-export default  {
-    components : {
-        Header,
-        Balance,
-        IncomeExpense,
-        TransactionList,
-        AddTransaction
-    }
-}
+
+import {ref} from "vue";
+
+const transactions = ref( [
+    { id: 1, text: 'Flower', amount: -20 },
+    { id: 2, text: 'Salary', amount: 300 },
+    { id: 3, text: 'Book', amount: -10 },
+    { id: 4, text: 'Camera', amount: 150 },
+    ]);
 </script>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -11,17 +11,15 @@
     </ul>
 </template>
 
-<script>
-export default {
-    data() {
-        return {
-         transactions: [
-   { id: 1, text: 'Flower', amount: -20 },
-   { id: 2, text: 'Salary', amount: 300 },
-  { id: 3, text: 'Book', amount: -10 },
-   { id: 4, text: 'Camera', amount: 150 },
-            ]
-        }
-    }
-}
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+});
+
+
 </script>
