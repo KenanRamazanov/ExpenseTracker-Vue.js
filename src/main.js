2e680f61a172793e4ab2fbdf4a3ea6a3dfba8bf9
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-05 21:04:19 +0400

    Editing main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import './assets/style.css'
+import { createApp } from 'vue';
+imporcreateApp, t Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
+import './assets/style.css';
+import App from './App.vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(Toast);
+app.mount('#app');
